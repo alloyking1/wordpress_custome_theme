8a4fa2dafdb36c0c73e1374ad46fe13fc82d2005
alloyking1 <alloyking1@gmail.com>
2020-05-07 08:41:17 -0400

    added template, css, and customized the theme. showing now. workin on the navigation list

---
+++ wp-content/themes/customdesing/header.php
@@ -0,0 +1,39 @@
+<!--
+
+=========================================================
+* Argon Design System - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Design System for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    
+    <title><?php bloginfo('name'); ?> <?php wp_title('-'); ?></title>
+
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <!-- styles -->
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css"  />
+    
+    <!-- pull in style for plugins -->
+    <?php wp_head(); ?>
+    
+    <!-- Favicon -->
+    <link href="<?php bloginfo('template_directory') ?>/theme/assets/img/brand/favicon.png" rel="icon" type="image/png">
+ 
+</head>
+
+<body>
