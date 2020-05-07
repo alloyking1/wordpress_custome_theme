d699b22f418251989b3621f620be11098415cd90
alloyking1 <alloyking1@gmail.com>
2020-05-07 10:50:02 -0400

    displaying pages from the wordpress navigation on the index.php file

--- wp-content/themes/customdesing/index.php
+++ wp-content/themes/customdesing/index.php
@@ -1,11 +1,29 @@
-<?php get_header(); ?>
-    <main>
+<?php 
+    get_header(); 
+    
+    if(have_posts()):
+        while(have_posts()):the_post(); ?>
+
+        <article>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_content(); ?>
+        </article>
+    <?php endwhile;
+    else:
+
+    endif;
+
+
+    get_footer(); 
+?>
+
+<!-- <main>
         <section class="section section-lg">
             <div class="container">
                 <div class="row row-grid align-items-center">
-                <!-- <div class="col-md-6 order-md-2">
+                <div class="col-md-6 order-md-2">
                     <img src="../assets/img/theme/promo-1.png" class="img-fluid floating" alt="image">
-                </div> -->
+                </div>
                 <div class="col-md-10 order-md-1">
                     <div class="pr-md-5">
                     <h1 class="display-1">We create original content that engages and connects with people</h1>
@@ -16,5 +34,4 @@
                 </div>
             </div>
         </section>
-    </main>
-<?php get_footer(); ?>
+    </main> -->
